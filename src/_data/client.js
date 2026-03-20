2429682b1e9af13fa06a17039f4625aebb5a70bc
sarahsmaples <sarah@tallymeals.com>
2026-03-20 16:44:56 -0500

    Initial Commit

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: "Rocky Mountain Thai kitchen",
+  tagline: "Thai Restaurant in Leadville Colorado",
+  address: {
+    street: "422 Harrison Ave",
+    city: "Leadville",
+    state: "Colorado",
+    stateAbbr: "CO",
+    zip: "80461",
+  },
+  phone: "(719) 293-2029",
+  domain: "",
+  email: "phean1976@gmail.com",
+  servesCuisine: [],
+  priceRange: "",
+  reservationUrl: "",
+  social: {
+    instagram: "",
+    facebook: "",
+  },
+};
